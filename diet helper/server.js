745414089fa73c849a0c40fb9f76ecd2c227f5b3
Thomas <thomascarp3nt3r@gmail.com>
2026-02-10 12:14:51 -0500

    server side

---
+++ diet helper/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static("public"));
+
+// Serve quotes.json
+app.get("/quotes.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "quotes.json"));
+});
+
+// Root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "app.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
